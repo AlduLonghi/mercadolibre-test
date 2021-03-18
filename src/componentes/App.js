b56231d31e335681fd94b9ee1843b15278dced2c
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-18 12:10:27 -0300

    Añadir estilos para componente ItemLink

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,11 +1,17 @@
 import '../estilos/App.scss';
+import ItemLink from './ItemLink';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <header>
-      <Navbar />
-    </header>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <ItemLink />
+      </main>
+    </>
   );
 }
 
--- src/componentes/ItemLink.js
+++ src/componentes/ItemLink.js
@@ -1,18 +1,19 @@
 import Logo from '../assets/Logo_ML.png';
+import '../estilos/ItemLink.scss';
 
-const ItemLink = () => {
-    return ( 
-      <div className="itemLink-container">
-          <img src={Logo} alt="Item thumbnail"/>
-          <div>
-              <p>$1.900</p>
-              <p>Samsung Galaxy A51 128 GB prism crush black 4 GB RAM</p>
-          </div>
-          <div>
-              <p>Capital Federal</p>
-          </div>
+const ItemLink = () => (
+  <div className="itemLink-container">
+    <img src={Logo} alt="Item thumbnail" />
+    <div className="itemlink-detalles">
+      <div>
+        <p className="precio">$1.900</p>
+        <p className="descripcion">Samsung Galaxy A51 128 GB prism crush black 4 GB RAM drert rgtegge fgregetgrgb ewrfrefe</p>
       </div>
-    );
-}
- 
+      <div>
+        <p className="locacion">Capital Federal</p>
+      </div>
+    </div>
+  </div>
+);
+
 export default ItemLink;
