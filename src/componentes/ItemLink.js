398a60c7d7d36d2395e9db60a763a1fb79bfeced
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-18 18:38:49 -0300

    Instalar prop-types para manejar props validation

--- src/componentes/ItemLink.js
+++ src/componentes/ItemLink.js
@@ -1,16 +1,16 @@
 import Logo from '../assets/Logo_ML.png';
 import '../estilos/ItemLink.scss';
 
-const ItemLink = () => (
+const ItemLink = ({ itemInfo }) => (
   <div className="itemLink-container">
     <img src={Logo} alt="Item thumbnail" />
     <div className="itemlink-detalles">
       <div>
-        <p className="precio">$1.900</p>
-        <p className="descripcion">Samsung Galaxy A51 128 GB prism crush black 4 GB RAM drert rgtegge fgregetgrgb ewrfrefe</p>
+        <p className="precio">{itemInfo.price}</p>
+        <p className="descripcion">{itemInfo.title}</p>
       </div>
       <div>
-        <p className="locacion">Capital Federal</p>
+        <p className="locacion">{itemInfo.address.state_name}</p>
       </div>
     </div>
   </div>
--- src/vistas/Resultados.js
+++ src/vistas/Resultados.js
@@ -1,11 +1,24 @@
+import axios from 'axios';
+import { useState } from 'react';
 import ItemLink from '../componentes/ItemLink';
 import '../estilos/Resultados.scss';
 
-const Resultados = () => (
-  <div className="itemLinks-container">
-    <ItemLink />
-    <ItemLink />
-  </div>
-);
+const Resultados = () => {
+  const [resultados, setResultados] = useState();
+
+  async function fetchApi() {
+    const request = await axios.get('https://api.mercadolibre.com/sites/MLA/search?q=celular&limit=4', {})
+      .then(res => setResultados(res.data.results));
+    return request;
+  }
+
+  fetchApi();
+
+  return (
+    <div className="itemLinks-container">
+      {resultados.forEach(res => <ItemLink itemInfo={res} />)}
+    </div>
+  );
+};
 
 export default Resultados;
