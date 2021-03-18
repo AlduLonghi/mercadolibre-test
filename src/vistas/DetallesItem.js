a41e61a1c853f0813b56514e5929ce8bc79ce5f9
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-18 15:26:57 -0300

    Crear componente DetallesItem

---
+++ src/vistas/DetallesItem.js
@@ -0,0 +1,23 @@
+const DetallesItem = () => {
+    return ( 
+      <div>
+          <div>
+              <div>
+                  <img/>
+                  <div>
+                      <p></p>
+                      <h2></h2>
+                      <h1></h1>
+                      <button></button>
+                  </div>
+              </div>
+              <div>
+                  <h3></h3>
+                  <p></p>
+              </div>
+          </div>
+      </div>
+    );
+}
+ 
+export default DetallesItem;
