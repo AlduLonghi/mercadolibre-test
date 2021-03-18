78fff69e48ee9cd0bc6862a00f4efefa8caf70b7
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-18 15:35:09 -0300

    Crear stylesheet para componente DetallesItem

--- src/vistas/DetallesItem.js
+++ src/vistas/DetallesItem.js
@@ -1,23 +1,32 @@
-const DetallesItem = () => {
-    return ( 
+import Logo from '../assets/Logo_ML.png';
+
+const DetallesItem = () => (
+  <div className="detallesItem-container">
+    <div>
       <div>
-          <div>
-              <div>
-                  <img/>
-                  <div>
-                      <p></p>
-                      <h2></h2>
-                      <h1></h1>
-                      <button></button>
-                  </div>
-              </div>
-              <div>
-                  <h3></h3>
-                  <p></p>
-              </div>
-          </div>
+        <img src={Logo} alt="Imagen producto" />
+        <div>
+          <p>Nuevo - 234 vendidos</p>
+          <h2>deco reverse sombrero oxford</h2>
+          <h1>$1.980</h1>
+          <button type="button">comprar</button>
+        </div>
       </div>
-    );
-}
- 
+      <div>
+        <h3>descripción del producto</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+    </div>
+  </div>
+);
+
 export default DetallesItem;
