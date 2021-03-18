83a83c1271f499046bb0fdb3b82970b3de9156db
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-18 11:29:28 -0300

    Crear componente ItemLink

---
+++ src/componentes/ItemLink.js
@@ -0,0 +1,16 @@
+const ItemLink = () => {
+    return ( 
+      <div>
+          <img />
+          <div>
+              <p></p>
+              <p></p>
+          </div>
+          <div>
+              <p></p>
+          </div>
+      </div>
+    );
+}
+ 
+export default ItemLink;
