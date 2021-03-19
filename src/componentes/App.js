f6b16ec93b855fe2d5397322634c56f6d5262ce7
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-19 09:41:36 -0300

    Crear onChange y onClick functions para navbar

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,8 +1,20 @@
+import { useState, useHistory } from 'react';
 import '../estilos/App.scss';
 import Navbar from './Navbar';
 import Rutas from './Rutas';
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState();
+  const history = useHistory();
+
+  const handleChange = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleCLick = () => {
+    history.push(`/items?q=${query}`)
+  }
+
   return (
     <>
       <header>
