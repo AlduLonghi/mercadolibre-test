b7b6d0b5fce59aaa970cbdf9acc4061dbcd281c7
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-19 10:18:48 -0300

    TRansformar itemLink componente en link

--- src/componentes/ItemLink.js
+++ src/componentes/ItemLink.js
@@ -1,19 +1,22 @@
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../estilos/ItemLink.scss';
 
 const ItemLink = ({ itemInfo }) => (
-  <div className="itemLink-container">
-    <img src={itemInfo.thumbnail} alt="Item thumbnail" />
-    <div className="itemlink-detalles">
-      <div>
-        <p className="precio">{`$${itemInfo.price.toLocaleString('es-ES')}`}</p>
-        <p className="descripcion">{itemInfo.title}</p>
-      </div>
-      <div>
-        <p className="locacion">{itemInfo.address.state_name}</p>
+  <Link to={`/item/${itemInfo.id}`} className="item-link">
+    <div className="itemLink-container">
+      <img src={itemInfo.thumbnail} alt="Item thumbnail" />
+      <div className="itemlink-detalles">
+        <div>
+          <p className="precio">{`$${itemInfo.price.toLocaleString('es-ES')}`}</p>
+          <p className="descripcion">{itemInfo.title}</p>
+        </div>
+        <div>
+          <p className="locacion">{itemInfo.address.state_name}</p>
+        </div>
       </div>
     </div>
-  </div>
+  </Link>
 );
 
 ItemLink.propTypes = {
