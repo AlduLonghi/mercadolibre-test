aece91941d1d46d54926debf2d0ae801668df2d1
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-03-20 14:31:38 -0300

    Modificar componente DetallesItem endpoint

--- frontend/src/vistas/DetallesItem.js
+++ frontend/src/vistas/DetallesItem.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchItem } from '../helpers/api';
 import '../estilos/DetallesItem.scss';
 
 const DetallesItem = () => {
@@ -8,7 +7,13 @@ const DetallesItem = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    fetchItem(id).then(res => setItemData(res.data));
+    setFetching(true);
+    axios.get(`/api/items/${id}`, {})
+      .then(res => res.data)
+      .then(data => {
+        setItemData(data.item);
+        setFetching(false);
+      });
   }, []);
 
   let toRenderComponent;
@@ -19,26 +24,17 @@ const DetallesItem = () => {
       <div className="detallesItem-container">
         <div>
           <div className="detalles-inner-top">
-            <img src={itemData.thumbnail} alt="Imagen producto" />
+            <img src={itemData.picture} alt="Imagen producto" />
             <div>
-              <p className="estado">{`${condicion} - ${itemData.sold_quantity}`}</p>
+              <p className="estado">{`${condicion} - ${itemData.sold_quantity} vendidos`}</p>
               <h2>{itemData.title}</h2>
-              <h1 className="detalles-precio">{`$${itemData.price.toLocaleString('es-ES')}`}</h1>
+              <h1 className="detalles-precio">{`$${itemData.price.ammount.toLocaleString('es-ES')}`}</h1>
               <button type="button" className="comprar-btn">comprar</button>
             </div>
           </div>
           <div className="detalles-inner-bottom">
             <h3>Descripción del producto</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis aute irure dolor in reprehenderit
-              in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
+            <p>{itemData.description}</p>
           </div>
         </div>
       </div>
